76b2fdaaa4da609dc5fd68f4dda788a5faa8f721
lazytech614 <143270416+lazytech614@users.noreply.github.com>
2024-08-24 23:39:10 +0530

    Update PopularItems.jsx

--- src/components/Popular/PopularItems.jsx
+++ src/components/Popular/PopularItems.jsx
@@ -3,7 +3,7 @@ import { Item } from '../Item/Item'
 import { useNavigate } from 'react-router-dom'
 
 export const PopularItems = (props) => {
-  const baseURL = 'http://localhost:4000'
+  const baseURL = 'https://e-commerce-website-backend-uldm.onrender.com'
 
   const [popular, setpopular] = useState([]);
 
