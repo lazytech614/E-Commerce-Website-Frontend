fb1315d08e5a8acc8545e8dd0c526710aacce8b2
lazytech614 <143270416+lazytech614@users.noreply.github.com>
2024-08-25 03:31:37 +0530

    Update SignUpForm.jsx

--- src/components/Forms/SignUpForm.jsx
+++ src/components/Forms/SignUpForm.jsx
@@ -7,7 +7,7 @@ import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 
 export const SignUpForm = ({setIsOpenSignUpModal, setIsOpenSignInModal}) => {
-  const baseURL = 'https://e-commerce-website-backend-uldm.onrender.com'
+  const baseURL = 'https://e-commerce-website-backend-30l8.onrender.com'
   const navigate = useNavigate()
 
   const [formData, setFormData] = useState({
