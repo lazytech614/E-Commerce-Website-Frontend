2959a1dd5134e1940a59822eec887b7cec50331e
lazytech614 <143270416+lazytech614@users.noreply.github.com>
2024-08-25 03:31:19 +0530

    Update SignInForm.jsx

--- src/components/Forms/SignInForm.jsx
+++ src/components/Forms/SignInForm.jsx
@@ -5,7 +5,7 @@ import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 
 export const SignInForm = ({setIsOpenSignInModal}) => {
-  const baseURL = 'https://e-commerce-website-backend-ycpj.onrender.com'
+  const baseURL = 'https://e-commerce-website-backend-30l8.onrender.com'
 
   const [formData, setFormData] = useState({
     phone: "",
